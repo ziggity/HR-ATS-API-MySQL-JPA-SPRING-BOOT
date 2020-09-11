7b7b9d6de96e12fad27764248f4383522261d1e7
Zach Beecher <zachbeecher@gmail.com>
2020-09-11 16:08:34 -0700

    added index page /

---
+++ src/main/java/com/promineo/hrtool/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.promineo.hrtool.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello and welcome! Use http://localhost:8080/api/candidates to see all candidates";
+    }
+}
